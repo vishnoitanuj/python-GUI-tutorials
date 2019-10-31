53131a00acb0c2c99da54ccee7c8eb6d793b7627
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2019-10-31 11:58:39 +0530

    button created

--- intro.py
+++ intro.py
@@ -5,7 +5,27 @@ class Window(Frame):
     def __init__(self,master=None):
         Frame.__init__(self,master)
         self.master = master
+        self.init_windows()
+    
+    # Creation of init_windows
+    def init_windows(self):
+        
+        #title of window
+        self.master.title("GUI")
+
+        #allow the widget to take the full space of the root widget
+        self.pack(fill=BOTH, expand=1)
+
+        #creating a button instance
+        quitButton = Button(self, text="Quit")
+
+        #Place the button on the window
+        quitButton.place(x=0,y=0)
+
 
 root = Tk()
+
+#Size of the window
+root.geometry("400x300")
 app =Window(root)
 root.mainloop()
