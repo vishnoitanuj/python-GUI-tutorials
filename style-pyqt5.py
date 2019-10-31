d44d126cb461573551c188e40f86dfd1dde95e86
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2019-11-01 02:57:39 +0530

    color palettes done

--- style-pyqt5.py
+++ style-pyqt5.py
@@ -1,6 +1,8 @@
+#The coarsest way to change the appearance of your application is to set the global Style.
 #Everything you see in a (Py)Qt app is a widget: Buttons, labels, windows, dialogs, progress bars etc.
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import *
 app = QApplication([])
+app.setStyle('Fusion')
 window = QWidget()
 layout = QVBoxLayout()
 layout.addWidget(QPushButton('Top'))
